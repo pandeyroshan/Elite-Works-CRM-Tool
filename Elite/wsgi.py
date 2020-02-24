9dde6ddba0da4e192fee03a4cccc20fff44aeb69
Roshan Pandey <pandeyroshan556@gmail.com>
2020-02-24 21:54:52 +0530

    Production L1 Ready Code

--- Elite/wsgi.py
+++ Elite/wsgi.py
@@ -8,3 +8,12 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+
+
+
+'''
+
+Production Ready Code for Deployment 
+
+
+'''
