32e4bb0ec9783e91a8b6b67ce3aff28750be023c
Roshan Pandey <pandeyroshan556@gmail.com>
2020-02-24 19:14:40 +0530

    Production ready code

--- Elite/settings.py
+++ Elite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=n_&p(dnr7a&2u^^mnj(x0&7syd+f+5adbfb583y)av%@4$fiw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
