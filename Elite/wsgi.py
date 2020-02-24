0e2336701768846c845df334c9301e89cda043b6
Roshan Pandey <pandeyroshan556@gmail.com>
2020-02-24 21:41:33 +0530

    whitenose -> whitenoise

--- Elite/wsgi.py
+++ Elite/wsgi.py
@@ -5,6 +5,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Elite.settings')
 
 from django.core.wsgi import get_wsgi_application
-from whitenose.django import DjangoWhiteNoise
+from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
