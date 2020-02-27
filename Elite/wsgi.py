b438fb65027c71af1c672d79ea06848164a58bd6
Roshan Pandey <pandeyroshan556@gmail.com>
2020-02-27 18:54:10 +0530

    Server Related Tweaks

--- Elite/wsgi.py
+++ Elite/wsgi.py
@@ -4,7 +4,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Elite.settings')
 
-from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
